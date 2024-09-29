6d60eb2fefbacc470362e7506195c582da7cf161
DeynerZavala <dyr.zva.hyi@gmail.com>
2024-09-29 01:22:43 -0500

    combined

--- db-init-scripts/init-multiple-dbs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
-    CREATE DATABASE "PathwayEduM1";
-    CREATE DATABASE "PathwayEduM2";
-EOSQL
